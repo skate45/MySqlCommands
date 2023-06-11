4905c803f6cbf24b8e5d2b4b1439f0af336bccb7
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-11 17:22:06 -0600

    Update Regexp.sql

--- Regexp.sql
+++ Regexp.sql
@@ -1,5 +1,8 @@
 
-/*Phone validation*/
+/*
+Phone validation
+A record is correct if the IP it contains is a valid IPv4 address, and either the first or the second component in the host part is a two-digit number.
+*/
 SELECT
 name,
 surname,
@@ -10,3 +13,14 @@ OR REGEXP_LIKE(phone_number,'^\\(1\\)[0-9]{3}-[0-9]{3}-[0-9]{4}$')=1
 ORDER BY surname;
 
 /*ip validation*/
+SELECT
+	id,
+	ip
+	FROM ips
+	WHERE (
+		(
+			ip REGEXP('^[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{2}$')=1
+		) OR(
+			ip REGEXP('^[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{2}\.[0-9]{1,4}$')=1
+		)
+	)
