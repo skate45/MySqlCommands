b150d7c8ce5c1cc33ce8b443d1c872dabaa3bc84
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-11 14:41:25 -0600

    Redondeo inferior con Floor

--- ComandosMySql.sql
+++ ComandosMySql.sql
@@ -45,3 +45,17 @@ WHERE
 ORDER BY id;
   
 
+/*Redondeo inferior: Floor*/
+SELECT
+id,
+email_title,
+CASE
+  WHEN size<1048576 THEN
+    CONCAT(FLOOR(size/1024),' Kb')
+  ELSE
+    CONCAT(FLOOR(size/1048576),' Mb')
+END AS short_size
+FROM emails
+ORDER BY size DESC;
+
+
