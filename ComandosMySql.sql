96ede721b3c4762901502bab5e87948a00efc1ff
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-11 14:27:26 -0600

    Se agrega búsqueda binaria con like en texto

--- ComandosMySql.sql
+++ ComandosMySql.sql
@@ -10,3 +10,14 @@ FROM(
 )TB;
 
  
+/* Búsqueda en texto binario (attribute:string with text to search) */
+/*<one or more arbitrary character>%<first name>_<second name>%<zero or more arbitrary characters>*/
+SELECT
+*
+FROM users
+WHERE BINARY attribute LIKE CONCAT('%','\%',first_name,'_',second_name,'\%','%')
+  AND LENGTH(attribute)>LENGTH(first_name+second_name)+4
+  AND LEFT(attribute,1) NOT IN('%')
+
+ORDER BY attribute ASC;
+
