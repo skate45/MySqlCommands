9555743190a7c0f9dd2bc4fbfdb28e7441ae7d65
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-11 14:30:59 -0600

    SELECT CASE

--- ComandosMySql.sql
+++ ComandosMySql.sql
@@ -27,3 +27,21 @@ FROM answers
 ORDER BY id;
 
 
+/*SELECT CASE */
+SELECT
+id,
+a,
+b,
+operation,
+c
+FROM expressions
+WHERE 
+  CASE operation
+    WHEN '+' THEN a+b
+    WHEN '-' THEN a-b
+    WHEN '*' THEN a*b
+    WHEN '/' THEN a/b
+  END=c
+ORDER BY id;
+  
+
