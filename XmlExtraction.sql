68ef9c62f326276dfa59ee708f5b0c9d27f43a3a
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-11 14:37:38 -0600

    Update XmlExtraction.sql

--- XmlExtraction.sql
+++ XmlExtraction.sql
@@ -16,39 +16,39 @@
 */
 
 CREATE TEMPORARY TABLE Authors (
-		name varchar(255)
-	);
-	
-	SET @lim=(
-		SELECT COUNT(doc_id)
-		FROM catalogs
-	);
-	
-	SET @i=1;
-	
-	WHILE @i<=@lim DO
-		INSERT INTO Authors(name)
-		SELECT ExtractValue(
-			(SELECT
+	name varchar(255)
+);
+
+SET @lim=(
+	SELECT COUNT(doc_id)
+	FROM catalogs
+);
+
+SET @i=1;
+
+WHILE @i<=@lim DO
+	INSERT INTO Authors(name)
+	SELECT ExtractValue(
+		(SELECT
+		xml_doc
+		FROM(
+			SELECT
+			row_number() over(order by doc_id) as orden,
 			xml_doc
-			FROM(
-				SELECT
-				row_number() over(order by doc_id) as orden,
-				xml_doc
-				FROM catalogs
-			)TB WHERE orden=@i
-			)
-			, '//catalog/book[1]/author'
-		) as author;
-		
-		SET @i=@i+1;
-	END WHILE;
-	
-	SELECT
-	name as author
-	FROM Authors
-	GROUP BY name
-	ORDER BY name;
-	
-	DROP TABLE Authors;
+			FROM catalogs
+		)TB WHERE orden=@i
+		)
+		, '//catalog/book[1]/author'
+	) as author;
+
+	SET @i=@i+1;
+END WHILE;
+
+SELECT
+name as author
+FROM Authors
+GROUP BY name
+ORDER BY name;
+
+DROP TABLE Authors;
 	
