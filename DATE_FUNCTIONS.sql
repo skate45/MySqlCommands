92f51e611cbf3d06c999ad718f5cbb4eefa64653
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-18 04:34:54 -0600

    Add date_add and date_format functions

--- DATE_FUNCTIONS.sql
+++ DATE_FUNCTIONS.sql
@@ -33,4 +33,16 @@ GROUP BY YEAR(date),CASE
   WHEN MONTH(date) BETWEEN 10 AND 12 THEN 4
 END
 ORDER BY year ASC, quarter ASC;
+
   
+/*DATE FORMAT, DATE_ADD, AM/PM FORMAT 24/12 HRS */
+SELECT
+event_id,
+IF(
+  settings.hours=24,
+  DATE_FORMAT(DATE_ADD(date, INTERVAL timeshift MINUTE),'%Y-%m-%d %H:%i'),
+  DATE_FORMAT(DATE_ADD(date, INTERVAL timeshift MINUTE),'%Y-%m-%d %h:%i %p')
+) as formatted_date
+FROM events
+INNER JOIN settings ON events.user_id=settings.user_id
+ORDER BY event_id ASC;
