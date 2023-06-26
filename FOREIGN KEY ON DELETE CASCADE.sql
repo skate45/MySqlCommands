ddabaa5a97c17e9f35924e77a248784d7bacddf2
Ignacio Castillo <ignacioce3@gmail.com>
2023-06-25 18:57:28 -0600

    Update FOREIGN KEY ON DELETE CASCADE.sql

--- FOREIGN KEY ON DELETE CASCADE.sql
+++ FOREIGN KEY ON DELETE CASCADE.sql
@@ -2,7 +2,7 @@ ALTER TABLE groupcourses ADD FOREIGN KEY (course_id)
     REFERENCES courses(id) ON DELETE CASCADE;
 
     ALTER TABLE groupexams ADD FOREIGN KEY (course_id)
-    REFERENCES courses(id) ON DELETE CASCADE;
+    REFERENCES courses(id) ON DELETE CASCADE; --THIS DELETES THE FOREIGN TABLE rows
 
     DELETE FROM courses WHERE name LIKE '%-toremove';
 
@@ -13,3 +13,14 @@ ALTER TABLE groupcourses ADD FOREIGN KEY (course_id)
     SELECT group_id, course_id
       FROM groupexams
      ORDER BY group_id, course_id;
+
+
+
+--on DELETE SET NULL
+ALTER TABLE branches ADD FOREIGN KEY (branchtype_id)
+    REFERENCES branch_types(id) ON DELETE SET NULL; --THIS DO NOT DELETE THE FOREIGN TABLE rows BUT UPDATES THEM TO NULL
+
+    DELETE FROM branch_types WHERE name LIKE '%-outdated';
+
+    SELECT * FROM branches
+    ORDER BY branch_id;
