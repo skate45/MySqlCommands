4a9d0a7e07400b5811fc1022a1c8b19620ec821f
Ignacio Castillo <ignacioce3@gmail.com>
2023-07-01 22:28:22 -0600

    Update Distances x,y.sql

--- Distances x,y.sql
+++ Distances x,y.sql
@@ -25,4 +25,32 @@ where l.name < r.name
   and
   distance(point(l.x, l.y), point(r.x, r.y)) < 5
 order by 1, 2;
-        
+
+/*
+Minima distancia l->r
+*/
+SELECT
+	id1,
+	id2
+	FROM(
+		SELECT
+			id1,
+			id2,
+			ROW_NUMBER() OVER (PARTITION BY id1 ORDER BY DISTANCIA ASC) AS ORDEN_DISTANCIAS
+			/*
+			,DISTANCIA
+			*/
+		FROM(
+			SELECT
+			l.id as id1,
+			r.id as id2,
+			ROUND(
+				SQRT(POW(l.x - r.x,2) + POW(l.y - r.y,2))
+			, 2) AS DISTANCIA
+			FROM positions l
+			LEFT JOIN positions r ON (l.x<>r.x) OR (l.y<>r.y)
+		)TB
+	)TB2
+	WHERE ORDEN_DISTANCIAS=1
+	ORDER BY id1;
+
